3b9fc781f0be8bcc052d8341a217633c4c167d88
Nicholas Wiebe <digitalexistence18@gmail.com>
2022-02-17 23:13:24 -0700

    change address to heroku address

--- src/python_server.py
+++ src/python_server.py
@@ -2,7 +2,7 @@ from http.server import HTTPServer,  SimpleHTTPRequestHandler
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
   """Entrypoint for python server"""
-  server_address = ("0.0.0.0", 8000)
+  server_address = ("https://qr-python-server.herokuapp.com/", 8000)
   httpd = server_class(server_address, handler_class)
   print("launching server")
   httpd.serve_forever()
